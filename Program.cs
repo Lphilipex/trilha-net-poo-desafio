2e66b4c715c1c3bc562a7b0c48575be2778b6339
luiz philipe <79484293+Lphilipex@users.noreply.github.com>
2026-01-22 10:34:46 -0300

    Add program execution with Nokia and Iphone instances

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone("12","iph12","1283", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+
+
+Nokia nokia = new Nokia("123", "nokia2", "11875", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
